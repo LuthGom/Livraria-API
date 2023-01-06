b70b750c9693ca6404cfffdb1eff644699abfae8
LuthGom <lucianomendes23@yahoo.com>
2023-01-06 20:56:19 -0300

    Add odbConnection ao App para testar conexão: bem sucedida

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Router, Request, Response } from "express";
-
+import db from "./database/dbConnection";
 const app = express();
 
 const route = Router();
@@ -9,7 +9,7 @@ app.use(express.json());
 route.get("/", (req: Request, res: Response) => {
   res.json({ mensagem: "testando request" });
 });
-
+db;
 app.use(route);
 
 export default app;
