2cc34f253f5fd9d81bff9511bba6d166c3eea982
LuthGom <lucianomendes23@yahoo.com>
2023-01-07 16:05:40 -0300

    Deletando a requisição teste e importando a camada route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
-import express, { Router, Request, Response } from "express";
+import express from "express";
 import db from "./database/dbConnection";
+import route from "./routes/autorRoutes";
 const app = express();
-
-const route = Router();
 
 app.use(express.json());
 
-route.get("/", (req: Request, res: Response) => {
-  res.json({ mensagem: "testando request" });
-});
+
 db;
 app.use(route);
 
