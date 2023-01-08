079bbc24efb5fe78475faa5c2794a8bc0ebf379a
LuthGom <lucianomendes23@yahoo.com>
2023-01-08 16:28:32 -0300

    Repassando  nova routes para o app de forma correta

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import db from "./database/dbConnection";
-import route from "./routes/autorRoutes";
+import routes from "./routes/index";
 const app = express();
 
 app.use(express.json());
 
-
 db;
-app.use(route);
+routes(app);
 
 export default app;
