2e9d40f0a1bf2ed152e4dcd634d6a16ce79f450e
LuthGom <lucianomendes23@yahoo.com>
2023-01-08 15:28:21 -0300

    Criando uma camada de interfaces para cada model

--- src/interfaces/modelsInterfaces.ts
+++ src/interfaces/modelsInterfaces.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export interface IAutor {
   id: String;
   nome: String;
@@ -5,3 +7,45 @@ export interface IAutor {
   origem: String;
 }
 
+export interface ILivro {
+  id: String;
+  titulo: String;
+  autor: Object;
+  editora: Object;
+  volume: String;
+  numeroDePaginas: string;
+  isbn: Number;
+  generoLiterario: String;
+}
+
+export interface IEditora {
+  id: String;
+  nome: String;
+  cidade: String;
+  cnpj: String;
+}
+
+export interface IAdmin {
+  id: String;
+  nome: String;
+  usuario_admin: String;
+  senha: String;
+  email: String;
+}
+
+export interface ICliente {
+  id: String;
+  nome: String;
+  senha: String;
+  email: String;
+  endereco: Endereco
+}
+
+export type Endereco = {
+  rua: string;
+  numero: number;
+  bairro: string;
+  cep: number;
+  cidade: string;
+  uf: string;
+};
