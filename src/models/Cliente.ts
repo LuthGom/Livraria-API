975c8f59d6439fc194ae3135659fbb12a25f3e3c
LuthGom <lucianomendes23@yahoo.com>
2023-01-08 15:27:37 -0300

    Adicionando o campo Object de endereço e tornando os campos como obrigatórios

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { ICliente } from "../interfaces/modelsInterfaces";
 
 const { Schema } = mongoose;
 
-const clienteSchema = new Schema({
-  nome: String,
-  senha: String,
-  email: String,
+const clienteSchema = new Schema<ICliente>({
+  nome: { type: String, required: true },
+  senha: { type: String, required: true },
+  email: { type: String, required: true },
+  endereco: { type: Object, required: true },
 });
 
 export const Cliente = mongoose.model("client", clienteSchema);
