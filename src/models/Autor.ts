3521e1f2445d1166a76b97432b3764c02c06331b
LuthGom <lucianomendes23@yahoo.com>
2023-01-08 14:02:19 -0300

    Criando uma interface para a Model de Autor

--- src/models/Autor.ts
+++ src/models/Autor.ts
@@ -1,11 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
+
+interface IAutor {
+  id: String;
+  nome: String;
+  genero: String;
+  origem: String;
+}
 
 const { Schema } = mongoose;
 
-const autorSchema = new Schema({
+const autorSchema = new Schema<IAutor>({
+  id: String,
   nome: { type: String, required: true },
   genero: String,
   origem: String,
 });
 
-export const Autor = mongoose.model("autor", autorSchema);
+export const Autor = mongoose.model("author", autorSchema);
